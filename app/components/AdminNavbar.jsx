93c36d1424ccdb392d450dee8a1003cbbf5e298f
Gagandeep <igagandeep95@gmail.com>
2025-05-27 19:31:27 -0400

    fixed cors issue

--- app/components/AdminNavbar.jsx
+++ app/components/AdminNavbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
             : ''
         }`}
       >
-        <a href='/Portfolio'>
+        <a href='/'>
           <h1 className='text-4xl font-SigmarOne'>
             Gagan<span className='text-5xl text-red-700'>.</span>
           </h1>
